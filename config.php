3774398f7c13a0debb4d7bca1c9b47f822680ea3
Gabriel Rodriguez <gabriel.gurodriguez@gmail.com>
2020-02-07 11:23:12 -0500

    first release

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+// reCaptcha Setup
+// ===============
+
+// Mail Setup
+// ==========
+
+// Your Email Address where new emails will be sent to
+$to_email = 'contact@oxtan-tech.com';
+
+// Email Subject
+$mail_subject = 'Support Request';
+
+// Email content can be modified in the sendmail.php file.
+
+?>
